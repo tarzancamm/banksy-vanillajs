ae70ad7bb4c68b3ab9ff3ac2274ad67486377c11
Cameron Miner <cameronsminer@gmail.com>
2023-03-13 09:36:21 -0600

    Finished urban api request and parsed res data

--- client/index.js
+++ client/index.js
@@ -1,16 +1,36 @@
 const urbanForm = document.getElementById('urban-form')
 const urbanInput = document.querySelector('#urban-input')
+const urbanAnswer = document.querySelector('#urban-answer')
 
 
 const urbanHandler = (e) => {
     e.preventDefault()
 
     let param = urbanInput.value
-    console.log(param)
 
-    axios.get()
+    const options = {
+        method: 'GET',
+        url: 'https://mashape-community-urban-dictionary.p.rapidapi.com/define',
+        params: {term: param},
+        headers: {
+            'X-RapidAPI-Key': '7577c3e02cmsh371a88f3003f950p18dd14jsn1ca1dbe86336',
+            'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
+          }
+    }
+
+    axios.request(options)
         .then((res) => {
-            console.log(res.data)
+            console.log(res.data.list[0].definition)
+            let definition = res.data.list[0].definition
+
+            //! innerHTML option also works
+            // let answerCard = `<p>${definition}</p>`
+            // urbanAnswer.innerHTML += answerCard
+
+            const sentence = document.createElement('p')
+            sentence.textContent = definition
+            urbanAnswer.appendChild(sentence)
+
         })
         .catch((err) => {
             console.log(err)
